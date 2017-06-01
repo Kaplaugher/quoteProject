9c0b816f84ad4ee114443b23181e69e114340f26
Kaplaugher <kaplaugher94@gmail.com>
2017-06-01 09:34:17 -0400

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+    
+    </div>
+</template>
+
+<script>
+    export default {
+      data: function() {
+        return {
+          quotes: [
+              'Just a quote to seee something'
+          ],
+          maxQuotes: 10
+        }
+      }
+        
+    }
+</script>
+
+<style>
+</style>
